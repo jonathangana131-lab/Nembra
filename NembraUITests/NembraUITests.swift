e906049befd8f71bc786ce177ee54fa177fe7e64
jonathangana131-lab <jonathangana131@gmail.com>
2026-08-05 04:04:31 -0700

    test: cap Dashboard UI test execution time

--- NembraUITests/NembraUITests.swift
+++ NembraUITests/NembraUITests.swift
@@ -7,6 +7,7 @@ final class NembraUITests: XCTestCase {
     override func setUp() {
         super.setUp()
         continueAfterFailure = false
+        executionTimeAllowance = 60
         XCUIDevice.shared.orientation = .portrait
         app = XCUIApplication()
     }
