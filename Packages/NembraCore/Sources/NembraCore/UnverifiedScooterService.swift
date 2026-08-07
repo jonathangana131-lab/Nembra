d3ce2177f32356b0d149987119edf2f272f3f006
jonathangana131-lab <jonathangana131@gmail.com>
2026-08-07 00:41:28 -0700

    Use unverified command failure in production gate

--- Packages/NembraCore/Sources/NembraCore/UnverifiedScooterService.swift
+++ Packages/NembraCore/Sources/NembraCore/UnverifiedScooterService.swift
@@ -69,12 +69,12 @@ public actor UnverifiedScooterService: ScooterService {
         publish()
     }
 
-    public func setHeadlight(_ enabled: Bool) async throws { throw ScooterCommandError.unsupportedConfiguration }
-    public func setLocked(_ locked: Bool) async throws { throw ScooterCommandError.unsupportedConfiguration }
-    public func setCruise(_ enabled: Bool) async throws { throw ScooterCommandError.unsupportedConfiguration }
-    public func setRideMode(_ mode: RideMode) async throws { throw ScooterCommandError.unsupportedConfiguration }
-    public func setStartMode(_ mode: StartMode) async throws { throw ScooterCommandError.unsupportedConfiguration }
-    public func setSpeedLimit(kilometersPerHour: Int, slot: SpeedLimitSlot) async throws { throw ScooterCommandError.unsupportedConfiguration }
+    public func setHeadlight(_ enabled: Bool) async throws { throw ScooterCommandError.unverifiedConfiguration }
+    public func setLocked(_ locked: Bool) async throws { throw ScooterCommandError.unverifiedConfiguration }
+    public func setCruise(_ enabled: Bool) async throws { throw ScooterCommandError.unverifiedConfiguration }
+    public func setRideMode(_ mode: RideMode) async throws { throw ScooterCommandError.unverifiedConfiguration }
+    public func setStartMode(_ mode: StartMode) async throws { throw ScooterCommandError.unverifiedConfiguration }
+    public func setSpeedLimit(kilometersPerHour: Int, slot: SpeedLimitSlot) async throws { throw ScooterCommandError.unverifiedConfiguration }
 
     private func publish() {
         state.lastUpdated = .now
