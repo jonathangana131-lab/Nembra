a1d64baeb5c87517d8e61be99038d19d80198cc7
jonathangana131-lab <jonathangana131@gmail.com>
2026-08-08 14:55:17 -0700

    fix(capture): reject duplicate software-export JSON fields

--- Packages/NembraBluetoothCapture/Sources/NembraBluetoothCapture/PassiveBluetoothExperimentOneSoftwareExport.swift
+++ Packages/NembraBluetoothCapture/Sources/NembraBluetoothCapture/PassiveBluetoothExperimentOneSoftwareExport.swift
@@ -70,6 +70,7 @@ public enum PassiveBluetoothExperimentOneSoftwareExportError: Error, Equatable, 
     case manifestBuildMismatch
     case manifestTargetMismatch
     case malformedWireData
+    case duplicateWireField(String)
     case unexpectedWireField(String)
 }
 
@@ -394,6 +395,9 @@ public enum PassiveBluetoothExperimentOneSoftwareExportCodec {
     }
 
     private static func validateClosedWorldShape(_ data: Data) throws {
+        if let duplicateKey = PassiveBluetoothStrictJSON.duplicateTopLevelObjectKey(in: data) {
+            throw PassiveBluetoothExperimentOneSoftwareExportError.duplicateWireField(duplicateKey)
+        }
         guard let root = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else {
             throw PassiveBluetoothExperimentOneSoftwareExportError.malformedWireData
         }
