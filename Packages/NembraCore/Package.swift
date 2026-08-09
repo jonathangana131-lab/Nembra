6e97cdf4a5e0e88ad0ad0f7ddc522ebe00380238
jonathangana131-lab <jonathangana131@gmail.com>
2026-08-09 15:38:30 -0700

    fix(core): declare Synchronization platform floor

--- Packages/NembraCore/Package.swift
+++ Packages/NembraCore/Package.swift
@@ -3,6 +3,10 @@ import PackageDescription
 
 let package = Package(
     name: "NembraCore",
+    platforms: [
+        .iOS(.v18),
+        .macOS(.v15)
+    ],
     products: [
         .library(name: "NembraCore", targets: ["NembraCore"])
     ],
