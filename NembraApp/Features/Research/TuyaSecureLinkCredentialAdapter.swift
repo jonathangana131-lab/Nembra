20cdb227ecf3cb701b90539fd592f8984c4df985
jonathangana131-lab <jonathangana131@gmail.com>
2026-08-09 21:05:28 -0700

    capture: remove superseded credential adapter

--- NembraApp/Features/Research/TuyaSecureLinkCredentialAdapter.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension TuyaSecureLinkPreflightModel {
-    enum TuyaCaptureCredentialStore {
-        static func load() -> TuyaCaptureCredential? {
-            TuyaCaptureCredentialVault.load()
-        }
-    }
-}
